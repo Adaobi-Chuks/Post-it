a9bd42c0147ceefae98fe9fd1b05276b61e86b2d
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2023-03-11 14:26:09 +0100

    Feature: Add img-tag to user model and refactor where necessary

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -7,6 +7,7 @@ const createSchema = Joi.object({
     email: Joi.string().email().required().lowercase().trim(),
     password: Joi.string().required().min(6).max(50),
     avatarURL: Joi.forbidden(),
+    imageTag: Joi.forbidden(),
     //".iso" ensures that the dateOfBirth field is a valid ISO-formatted date
     dateOfBirth: Joi.date().iso().required(),
     bio: Joi.string().trim().allow(null).optional(),
@@ -22,6 +23,7 @@ const editSchema = Joi.object({
     email: Joi.string().email().optional().lowercase().trim(),
     password: Joi.string().optional().min(6).max(50),
     avatarURL: Joi.forbidden(),
+    imageTag: Joi.forbidden(),
     dateOfBirth: Joi.date().iso().optional(),
     bio: Joi.string().trim().allow(null).optional(),
     gender: Joi.string().optional().valid(ENUM.MALE, ENUM.FEMALE).trim(),
